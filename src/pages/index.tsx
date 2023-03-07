e39e14084fbbc68a5c0b0d50d1bf9ee7557f7f0a
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-03-07 12:29:45 +0000

    update: change width of cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,12 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-green-400 via-cyan-500 to-blue-500 flex flex-col">
           <Summary />
-          <div className="bg-white rounded shadow w-[40rem] h-[32rem] mx-auto my-8 relative -top-[8rem] -mb-[6rem]">
-            asdddf
+          <div className="bg-white rounded shadow w-[72rem] h-[32rem] mx-auto my-8 p-8">
+            <div className="text-2xl text-green-500 font-bold">제목</div>
           </div>
-          <div className="bg-white rounded shadow w-[40rem] h-[32rem] mx-auto my-8">asdddf</div>
-          <div className="bg-white rounded shadow w-[40rem] h-[32rem] mx-auto my-8">asdddf</div>
+          <div className="bg-white rounded shadow w-[72rem] h-[32rem] mx-auto my-8 p-8">
+            <div className="text-2xl text-green-500 font-bold">제목</div>
+          </div>
         </div>
       </main>
     </>
