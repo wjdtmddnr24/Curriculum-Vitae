3c88e6846264afb5954c6721e8766e3d7e421b1d
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-03-13 14:22:43 +0000

    update: add more tech skills

--- src/features/TechSkills.tsx
+++ src/features/TechSkills.tsx
@@ -1,21 +1,35 @@
+import { faHouseChimneyCrack } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function TechSkills() {
   return (
     <div className="bg-white rounded shadow w-[80rem] min-h-[32rem] mx-auto my-8 relative -top-24 -mb-[4rem] p-8">
-      <div className="text-2xl text-transparent  bg-clip-text bg-gradient-to-br from-green-400 to-blue-500 font-bold mb-4">
+      <div className="text-2xl text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500 font-bold mb-4">
         Tech Skills
       </div>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-4 gap-8">
         <div>
           <div className="text-xl font-bold">General</div>
-          <div>
-            <ul>Linux</ul>
-            <ul>Docker</ul>
-            <ul>Kubernetes</ul>
-            <ul>Git</ul>
-            <ul>C/C++</ul>
-            <ul>Flutter / Dart</ul>
-            <ul>Android / Java</ul>
-          </div>
+          <ul>
+            <li>Linux</li>
+            <li>Bash/Zsh</li>
+            <li>Docker</li>
+            <li>Kubernetes</li>
+            <li>Git</li>
+            <li>C/C++</li>
+            <li>AWS</li>
+            <li>Algorithm & Problem Solving</li>
+          </ul>
+        </div>
+        <div>
+          <div className="text-xl font-bold">Coding Environment</div>
+          <ul>
+            <li>Visual Studio</li>
+            <li>Visual Studio Code</li>
+            <li>Intellij IDEA Series</li>
+            <li>Vim</li>
+            <li>Dev Container</li>
+          </ul>
         </div>
         <div>
           <div className="text-xl font-bold">Web Frontend</div>
@@ -24,11 +38,10 @@ export function TechSkills() {
               <li>React</li>
               <li>NextJS</li>
               <li>Tailwind</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
+              <li>HTML & CSS & JavaScript</li>
               <li>TypeScript</li>
               <li>SCSS</li>
+              <li>Styled Components / Emotion</li>
               <li>Redux</li>
               <li>Zustand</li>
               <li>Ant Design</li>
@@ -49,12 +62,32 @@ export function TechSkills() {
           </div>
         </div>
         <div>
+          <div className="text-xl font-bold">Mobile</div>
+          <div>
+            <ul>
+              <li>Flutter / Dart</li>
+              <li>Android / Java</li>
+            </ul>
+          </div>
+        </div>
+        <div>
           <div className="text-xl font-bold">Database</div>
           <div>
             <ul>
               <li>MySQL</li>
               <li>Mongoose</li>
               <li>PostgreSQL</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="text-xl font-bold">DevOps</div>
+          <div>
+            <ul>
+              <li>Kubernetes</li>
+              <li>Helm Charts</li>
+              <li>Argo CD</li>
+              <li>Gitlab CI</li>
             </ul>
           </div>
         </div>
