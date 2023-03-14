625d9a4c8a247929836ab892f3d40011feb96170
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-03-14 11:42:48 +0000

    update: add contents to Projects Card

--- src/features/Projects.tsx
+++ src/features/Projects.tsx
@@ -4,7 +4,11 @@ export function Projects() {
       <div className="text-2xl text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500 font-bold mb-4">
         Projects
       </div>
-      <div className="grid grid-cols-4 gap-8">Content</div>
+      <div className="grid grid-cols-4 gap-8">
+        <div>Webox - 퍼스널 모빌리티를 위한 클라우드 기반 블랙박스 어플리케이션</div>
+        <div>Captain Hook - Webhook 기반 알림 서비스</div>
+        <div>Captain Hook - Webhook 기반 알림 서비스</div>
+      </div>
     </div>
   );
 }
