dcf129bdd3eb6f7c03e9aba328c31a39549e0007
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-03-14 13:39:16 +0000

    update: adjust styles & contents in Summary

--- src/features/Summary.tsx
+++ src/features/Summary.tsx
@@ -9,7 +9,7 @@ export function Summary() {
   return (
     <div className="mx-auto flex justify-center items-center min-h-screen">
       <div className="bg-white w-[80rem] h-[36rem] grid grid-cols-[auto_1fr] rounded shadow-2xl">
-        <div className="w-[26rem] items-center justify-center flex flex-col border-r pt-12 pb-6">
+        <div className="w-[26rem] items-center flex flex-col border-r pt-16 pb-6">
           <div className="relative flex justify-center items-center w-72 h-72 group rounded-full">
             <Image
               className="z-10 w-72 h-72 my-auto rounded-full object-cover self-center justify-self-center transition-transform ease-out"
@@ -18,7 +18,7 @@ export function Summary() {
             />
             <div className="absolute transition-all duration-300 w-[18rem] h-[18rem] rounded-full group-hover:w-[calc(18rem+12px)] group-hover:h-[calc(18rem+12px)] bg-gradient-to-br from-green-400 to-blue-500 animate-spin"></div>
           </div>
-          <div className="w-full my-auto rounded-b justify-self-center flex flex-col items-center justify-center y-4">
+          <div className="w-full mt-12 rounded-b justify-self-center flex flex-col items-center justify-center y-4">
             <div className="text-3xl mb-1">정승욱</div>
             <div className="text-lg mb-0.5 text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500 font-bold">
               소프트웨어 엔지니어
@@ -36,7 +36,7 @@ export function Summary() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-2 p-8">
+        <div className="grid grid-cols-2 p-8 pt-16">
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Info</div>
             <ul className="text-slate-600">
@@ -58,9 +58,18 @@ export function Summary() {
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Career</div>
             <ul>
-              <li>2022~2023 공군 교육훈련매체개발병</li>
-              <li>2018~2021 코딩학원 강사</li>
-              <li>2018~2019 스타트업 안드로이드 개발자</li>
+              <li>
+                <div>2022~2023</div>
+                공군 교육훈련매체개발병
+              </li>
+              <li>
+                <div>2018~2021</div>
+                코딩학원 강사
+              </li>
+              <li>
+                <div>2018~2019</div>
+                스타트업 안드로이드 개발자
+              </li>
             </ul>
           </div>
           <div>
@@ -76,10 +85,15 @@ export function Summary() {
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Certificates</div>
             <ul>
-              <li>2021 정보처리산업기사 </li>
-              <li>2017 정보기기운용기능사 </li>
-              <li>2016 정보처리기능사 자격증 </li>
-              <li>2016 OCJP</li>
+              <li>
+                <div>2021</div> 정보처리산업기사
+              </li>
+              <li>
+                <div>2017</div> 정보기기운용기능사
+              </li>
+              <li>
+                <div>2016</div> 정보처리기능사 <br /> Oracle Certified Java Programmer
+              </li>
             </ul>
           </div>
         </div>
