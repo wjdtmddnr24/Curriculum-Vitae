de755b357045329b4475447402b5b41cad3a67fd
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-03-15 12:41:01 +0000

    update: change all contents to IconList

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,5 +1,13 @@
-import { faCalendar, faEnvelope, faJetFighterUp, faLocationDot, faSchoolFlag } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faChalkboardTeacher,
+  faCode,
+  faEnvelope,
+  faGraduationCap,
+  faJetFighterUp,
+  faLocationDot,
+  faSchoolFlag,
+} from "@fortawesome/free-solid-svg-icons";
 import { IconList } from "../icon-list/IconList";
 import { ProfileHeader } from "./ProfileHeader";
 
@@ -11,7 +19,7 @@ export function Profile() {
         <div className="grid grid-cols-2 p-16 pr-0 text-slate-600">
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Info</div>
-            <IconList>
+            <IconList className="space-y-0.5">
               <IconList.Item icon={faLocationDot}>서울, 한국</IconList.Item>
               <IconList.Item icon={faCalendar}>25살, 1999년생</IconList.Item>
               <IconList.Item icon={faEnvelope}>
@@ -22,27 +30,18 @@ export function Profile() {
           </div>
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Career</div>
-            <ul>
-              <li>
-                <div>2022~2023</div>
-                공군 교육훈련매체개발병
-              </li>
-              <li>
-                <div>2018~2021</div>
-                코딩학원 강사
-              </li>
-              <li>
-                <div>2018~2019</div>
-                스타트업 안드로이드 개발자
-              </li>
-            </ul>
+            <IconList className="space-y-0.5">
+              <IconList.Item icon={faJetFighterUp}>공군 교육훈련매체개발병 (2022~2023)</IconList.Item>
+              <IconList.Item icon={faChalkboardTeacher}>코딩학원 강사 (2018~2021)</IconList.Item>
+              <IconList.Item icon={faCode}>이비온(evon) 안드로이드 개발자 (2018~2019)</IconList.Item>
+            </IconList>
           </div>
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Education</div>
-            <IconList>
-              <IconList.Item icon={faSchoolFlag}>숭실대학교 졸업 예정</IconList.Item>
-              <IconList.Item icon={faSchoolFlag}>선린인터넷고등학교 졸업</IconList.Item>
-              <IconList.Item icon={faSchoolFlag}>
+            <IconList className="space-y-0.5">
+              <IconList.Item icon={faSchoolFlag}>숭실대학교 4학년 재학</IconList.Item>
+              <IconList.Item icon={faGraduationCap}>선린인터넷고등학교 졸업</IconList.Item>
+              <IconList.Item icon={faGraduationCap}>
                 Kunming International Academy &
                 <br />
                 언주중학교 졸업
@@ -52,17 +51,12 @@ export function Profile() {
 
           <div>
             <div className="text-gray-800 font-bold text-xl mb-2">Certificates</div>
-            <ul>
-              <li>
-                <div>2021</div> 정보처리산업기사
-              </li>
-              <li>
-                <div>2017</div> 정보기기운용기능사
-              </li>
-              <li>
-                <div>2016</div> 정보처리기능사 <br /> Oracle Certified Java Programmer
-              </li>
-            </ul>
+            <IconList>
+              <IconList.Item>정보처리산업기사 (2021)</IconList.Item>
+              <IconList.Item>정보기기운용기능사 (2017)</IconList.Item>
+              <IconList.Item>정보처리기능사 (2016)</IconList.Item>
+              <IconList.Item>Oracle Certified Java Programmer (2016)</IconList.Item>
+            </IconList>
           </div>
         </div>
       </div>
