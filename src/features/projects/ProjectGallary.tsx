5a8b61b5b5fb70c4d61131b7f115c0200cf8b3e3
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-04-08 11:33:18 +0000

    update: change gallary item style

--- src/features/projects/ProjectGallary.tsx
+++ src/features/projects/ProjectGallary.tsx
@@ -1,15 +1,16 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
 import portfolioPic from "../../../public/portfolio_combined_compressed.png";
 
 export const ProjectGallary = () => {
   return (
-    <div className="grid grid-cols-3 gap-8 text-lg">
+    <div className="grid grid-cols-3 gap-4 text-lg">
       <GallaryItem
-        title="Portfolio Website"
-        description="포트폴리오 웹사이트"
+        title="Portfolio"
+        description="포트폴리오 웹페이지"
         year={2023}
-        techs={[]}
-        link="#"
+        techs={["React", "Nextjs"]}
         imageURL={portfolioPic}
         sourceURL="#"
       />
@@ -18,8 +19,7 @@ export const ProjectGallary = () => {
         title="Webox"
         description="퍼스널 모빌리티를 위한 클라우드 기반 블랙박스 애플리케이션"
         year={2021}
-        techs={[]}
-        link="#"
+        techs={["React", "Nextjs"]}
         imageURL=""
         sourceURL="#"
       />
@@ -28,8 +28,7 @@ export const ProjectGallary = () => {
         title="Captain Hook"
         description="Webhook 기반 알림 서비스"
         year={2021}
-        techs={[]}
-        link="#"
+        techs={["React", "Nextjs"]}
         imageURL=""
         sourceURL="#"
       />
@@ -38,8 +37,7 @@ export const ProjectGallary = () => {
         title="SWM Chatbot"
         description="소프트웨어 마에스트로 유저 검색과 멘토링 알림 챗봇"
         year={2021}
-        techs={[]}
-        link="#"
+        techs={["React", "Nextjs"]}
         imageURL=""
         sourceURL="#"
       />
@@ -48,8 +46,7 @@ export const ProjectGallary = () => {
         title="Camtact"
         description="원격 회전캠을 이용한 비대면 감독 플랫폼"
         year={2020}
-        techs={[]}
-        link="#"
+        techs={["React", "Nextjs"]}
         imageURL=""
         sourceURL="#"
       />
@@ -58,8 +55,7 @@ export const ProjectGallary = () => {
         title="TCQR (Text Compressed QRCode)"
         description="문자열 압축기반 QR코드 애플리케이션"
         year={2016}
-        techs={[]}
-        link="#"
+        techs={["React", "Nextjs"]}
         imageURL=""
         sourceURL="#"
       />
@@ -73,25 +69,36 @@ interface GallaryItemProps {
   year: number;
   techs: string[];
   imageURL: StaticImageData | string;
-  link: string;
   sourceURL: string;
 }
-const GallaryItem = ({ title, year, description, imageURL }: GallaryItemProps) => {
+const GallaryItem = ({ title, year, description, imageURL, techs }: GallaryItemProps) => {
   return (
-    <div className="bg-slate-100 dark:bg-slate-700 rounded shadow hover:shadow-md group">
+    <div className="group relative">
       <Image
-        className="w-full aspect-[6/3.5] bg-slate-300 rounded-t object-cover filter brightness-75 group-hover:brightness-100 transition-all duration-500"
+        className="w-full aspect-[5/4] bg-slate-300 object-cover brightness-75 transition-all duration-500"
         src={imageURL}
         alt=""
         priority
       />
-      <div className=""></div>
-      <div className="p-4">
+      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-500 flex justify-center items-center group-hover:bg-gray-800/50">
+        <span className="text-white py-2 px-4 border border-white rounded-md cursor-pointer hover:bg-gray-100/30 transition-colors">
+          자세히 보기
+        </span>
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 text-slate-50 p-4">
+        <span className="ml-auto text-base">{year}</span>
+
         <div className="flex">
           <span className="text-xl font-bold">{title}</span>
-          <span className="ml-auto">{year}</span>
         </div>
-        <div>{description}</div>
+        <div className="mb-2 text-base">{description}</div>
+        <div className="flex items-center gap-2">
+          {techs.map((t) => (
+            <div className="text-base rounded-full" key={t}>
+              <div className="">{t}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
