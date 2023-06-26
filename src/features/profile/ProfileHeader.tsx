59b3401eb545dd4c911514aed908d4ec503fb592
wjdtmddnr24 <wjdtmddnr24@naver.com>
2023-06-26 16:50:35 +0000

    update: chage style to name

--- src/features/profile/ProfileHeader.tsx
+++ src/features/profile/ProfileHeader.tsx
@@ -18,7 +18,7 @@ export function ProfileHeader() {
         <div className="absolute transition-all duration-300 w-[20rem] h-[20rem] rounded-full group-hover:w-[calc(20rem+13px)] group-hover:h-[calc(20rem+13px)] bg-gradient-to-br from-green-400 to-blue-500 dark:from-pink-500 dark:via-purple-500 dark:to-indigo-500 animate-spin"></div>
       </div>
       <div className="w-full mt-12 rounded-b justify-self-center flex flex-col items-center justify-center y-4">
-        <div className="dark:text-slate-200 text-4xl mb-4">정승욱</div>
+        <div className="dark:text-slate-200 text-4xl mb-4 font-semibold">정승욱</div>
         <div className="text-xl mb-1 text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500 dark:from-pink-500 dark:via-purple-500 dark:to-indigo-500 font-bold">
           소프트웨어 엔지니어
         </div>
