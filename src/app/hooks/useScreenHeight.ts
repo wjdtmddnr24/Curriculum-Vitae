3d456f87e4247d55105799fd1c74bde2056573b9
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-01-18 03:11:04 +0000

    update: Refactor useScreenHeight hook and add useDebouncedScreenHeight hook

--- src/app/hooks/useScreenHeight.ts
+++ src/app/hooks/useScreenHeight.ts
@@ -1,17 +1,36 @@
-import { useEffect, useState } from "react";
+import _ from "lodash";
+import { useEffect, useRef, useState } from "react";
 
-export const useScreenHeight = () => {
-  const [screenHeight, setScreenHeight] = useState<number>(0);
+export const useScreenHeightRef = () => {
+  const screenHeightRef = useRef<number>(0);
+
   useEffect(() => {
-    setScreenHeight(window.innerHeight);
+    screenHeightRef.current = window.innerHeight;
 
     const onResize = () => {
-      setScreenHeight(window.innerHeight);
+      screenHeightRef.current = window.innerHeight;
     };
+
     window.addEventListener("resize", onResize);
 
     return () => window.removeEventListener("resize", onResize);
   }, []);
+
+  return screenHeightRef;
+};
+
+export const useDebouncedScreenHeight = (wait: number) => {
+  const [screenHeight, setScreenHeight] = useState<number>(0);
+
+  useEffect(() => {
+    setScreenHeight(window.innerHeight);
+    const onResize = _.debounce(() => {
+      setScreenHeight(window.innerHeight);
+    }, wait);
+    window.addEventListener("resize", onResize);
+
+    return () => window.removeEventListener("resize", onResize);
+  }, [wait]);
 
   return screenHeight;
 };
