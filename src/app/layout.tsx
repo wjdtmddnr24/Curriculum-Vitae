28b0672ee46789ff3daba8da738bdba50baafa8f
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-03-21 07:41:27 +0000

    update: apply z-index to background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={pretendardFont.className} suppressHydrationWarning>
       <body>
         <Providers>
-          <div className="fixed inset-0 bg-fixed bg-gradient-to-br from-pink-800 via-purple-800 to-indigo-800 opacity-0 dark:opacity-100 transition-opacity duration-500"></div>
-          <div className="fixed inset-0 bg-fixed bg-gradient-to-br from-green-400 via-cyan-500 to-blue-500 dark:opacity-0 transition-opacity duration-500"></div>
+          <div className="fixed inset-0 -z-10 bg-fixed bg-gradient-to-br from-pink-800 via-purple-800 to-indigo-800 opacity-0 dark:opacity-100 transition-opacity duration-500"></div>
+          <div className="fixed inset-0 -z-10 bg-fixed bg-gradient-to-br from-green-400 via-cyan-500 to-blue-500 dark:opacity-0 transition-opacity duration-500"></div>
           {children}
         </Providers>
         <SpeedInsights />
