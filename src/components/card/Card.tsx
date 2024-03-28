d759a6b796be3c09637c959a91da7256f6cb0b94
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-03-28 11:42:47 +0000

    update: add color transition to card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,7 +10,7 @@ const Card = ({ className, children }: CardProps) => {
   return (
     <div
       className={classNames([
-        "lg:w-[64rem] lg:aspect-[2/1] bg-white dark:bg-slate-800 lg:rounded-3xl lg:shadow-2xl",
+        "lg:w-[64rem] lg:aspect-[2/1] bg-white dark:bg-slate-800 lg:rounded-3xl lg:shadow-2xl transition-colors duration-500",
         className,
       ])}
     >
