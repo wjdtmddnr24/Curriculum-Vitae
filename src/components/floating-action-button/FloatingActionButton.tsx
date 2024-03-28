8869756b3134ee7592eff9d951cf9dd06cf238c4
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-03-28 11:48:45 +0000

    update: change shadow to drop-shadow

--- src/components/floating-action-button/FloatingActionButton.tsx
+++ src/components/floating-action-button/FloatingActionButton.tsx
@@ -6,7 +6,7 @@ interface FloatingActionButtonProps {
 function FloatingActionButton({ children, onClick }: FloatingActionButtonProps) {
   return (
     <button
-      className="size-14 rounded-full bg-white dark:bg-slate-900 shadow-lg z-50 flex items-center justify-center text-2xl text-blue-500 dark:text-indigo-500 cursor-pointer hover:shadow-xl hover:scale-110 ease-in-out active:scale-105 duration-200"
+      className="size-14 rounded-full bg-white dark:bg-slate-900 drop-shadow-lg z-50 flex items-center justify-center text-2xl text-blue-500 dark:text-indigo-500 cursor-pointer hover:shadow-xl hover:scale-110 ease-in-out active:scale-105 duration-200"
       onClick={onClick}
     >
       {children}
