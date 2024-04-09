0fc8b040e9774bebe61b1fd4cbf0e90604aba336
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-04-09 04:28:16 +0000

    update: change GallaryItem style

--- src/features/project-gallary/GallaryItem.tsx
+++ src/features/project-gallary/GallaryItem.tsx
@@ -15,36 +15,52 @@ interface GallaryItemProps {
 
 const GallaryItem = ({ title, year, description, imageURL, techs }: GallaryItemProps) => {
   return (
-    <Link href="#" className="group" draggable={false}>
+    <Link
+      href="#"
+      draggable={false}
+      className="hover:bg-gray-100 dark:hover:bg-gray-700 -m-2 p-2 rounded-xl transition-colors duration-200"
+    >
       <div className="flex mb-2 gap-2">
         <div className="grid overflow-hidden">
           <GradientTypography className="w-max text-xl font-bold truncate">{title}</GradientTypography>
           <div className="text-xs truncate">{description}</div>
         </div>
-        <div className="ml-auto text-sm self-center shrink-0 mx-2 flex gap-1 items-center">
+        <div className="lg:hidden ml-auto text-sm self-center shrink-0 mx-2 flex gap-1 items-center">
           <span>더보기</span>
           <FontAwesomeIcon icon={faArrowRight} />
         </div>
       </div>
-      <ImageHolder imageURL={imageURL} />
+      <ThumbnailHolder title={title} imageURL={imageURL} />
     </Link>
   );
 };
 
 export default GallaryItem;
 
-const ImageHolder = ({ imageURL }: { imageURL: StaticImageData | string | undefined }) => {
-  return imageURL ? (
-    <div className="relative overflow-hidden rounded-3xl">
-      <Image
-        className="w-full aspect-[304/193] bg-slate-300 object-cover group-hover:scale-105 transition-transform duration-500"
-        src={imageURL}
-        alt=""
-        priority
-        draggable={false}
-      />
+const ThumbnailHolder = ({ title, imageURL }: { imageURL: StaticImageData | string | undefined; title: string }) => {
+  if (imageURL) {
+    return (
+      <div className="relative overflow-hidden rounded-3xl">
+        <Image
+          className="w-full aspect-[304/193] bg-slate-300 dark:bg-slate-500 object-cover"
+          src={imageURL}
+          alt="thmbnail"
+          priority
+          draggable={false}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full aspect-[304/193] rounded-3xl relative overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-br from-green-400 via-cyan-500 to-blue-500 dark:opacity-0 transition-opacity duration-500"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-pink-800 via-purple-800 to-indigo-800 opacity-0 dark:opacity-100 transition-opacity duration-500"></div>
+      <div className="absolute inset-0 flex justify-center items-center">
+        <span className="text-4xl font-bold text-center text-white dark:text-slate-200 transition-colors duration-500">
+          {title}
+        </span>
+      </div>
     </div>
-  ) : (
-    <div className="w-full aspect-[304/193] bg-slate-300 object-cover rounded-3xl" />
   );
 };
