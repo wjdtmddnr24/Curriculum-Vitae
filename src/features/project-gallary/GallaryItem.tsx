22c12af3e5401673494fea028345d35ed9dde026
wjdtmddnr24 <wjdtmddnr24@naver.com>
2024-04-09 09:48:28 +0000

    update: chagne GallaryItem style

--- src/features/project-gallary/GallaryItem.tsx
+++ src/features/project-gallary/GallaryItem.tsx
@@ -18,7 +18,7 @@ const GallaryItem = ({ title, year, description, imageURL, techs }: GallaryItemP
     <Link
       href="#"
       draggable={false}
-      className="hover:bg-gray-100 dark:hover:bg-gray-700 -m-2 p-2 rounded-xl transition-colors duration-200"
+      className="hover:bg-gray-100 dark:hover:bg-gray-700 -m-2 p-2 rounded-xl transition-colors duration-200 group"
     >
       <div className="flex mb-2 gap-2">
         <div className="grid overflow-hidden">
@@ -40,23 +40,21 @@ export default GallaryItem;
 const ThumbnailHolder = ({ title, imageURL }: { imageURL: StaticImageData | string | undefined; title: string }) => {
   if (imageURL) {
     return (
-      <div className="relative overflow-hidden rounded-3xl">
-        <Image
-          className="w-full aspect-[304/193] bg-slate-300 dark:bg-slate-500 object-cover"
-          src={imageURL}
-          alt="thmbnail"
-          priority
-          draggable={false}
-        />
-      </div>
+      <Image
+        className="w-full aspect-[304/193] bg-slate-300 dark:bg-slate-500 object-cover dark:brightness-95 dark:group-hover:brightness-100 transition-all duration-500 outline outline-1 outline-slate-200 dark:outline-slate-700 rounded-3xl"
+        src={imageURL}
+        alt="thmbnail"
+        priority
+        draggable={false}
+      />
     );
   }
 
   return (
-    <div className="w-full aspect-[304/193] rounded-3xl relative overflow-hidden">
+    <div className="w-full aspect-[304/193] rounded-3xl relative outline outline-1 outline-slate-200 dark:outline-slate-700 overflow-hidden">
       <div className="absolute inset-0 bg-gradient-to-br from-green-400 via-cyan-500 to-blue-500 dark:opacity-0 transition-opacity duration-500"></div>
       <div className="absolute inset-0 bg-gradient-to-br from-pink-800 via-purple-800 to-indigo-800 opacity-0 dark:opacity-100 transition-opacity duration-500"></div>
-      <div className="absolute inset-0 flex justify-center items-center">
+      <div className="absolute inset-0 flex justify-center items-center ">
         <span className="text-4xl font-bold text-center text-white dark:text-slate-200 transition-colors duration-500">
           {title}
         </span>
